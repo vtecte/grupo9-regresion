75704db8433b96910b50a0fe5fffe4feecfe382c
vtecte <vicente.corts@uc.cl>
2026-05-15 11:18:08 -0400

    Agrege plot de Area quemada en el tiempo

--- analisis.R
+++ analisis.R
@@ -14,5 +14,14 @@ head(datos)
 # 2. ANÁLISIS EXPLORATORIO DE DATOS (EDA)
 #-------------------------------------------------------------------------------
 
+summary(datos)
+
+plot(datos$area, type = "l", 
+     main = "Área quemada en el tiempo",
+     ylab = "Área", xlab = "Índice Temporal (Meses)",
+     col = "blue", lwd = 1.5)
+
+
+
 
 
